5ce68fd9d0abd973631bb2168b5e5d305b410d9a
marisamak <marinasamakova1507@gmail.com>
2024-10-20 16:04:29 +0300

    add

--- 3/main.py
+++ 3/main.py
@@ -9,6 +9,13 @@ KEY_S = 83
 KEY_A = 65
 KEY_D = 68
 
+FPS = 60 # частота кадров
+
+def update():
+    player.update()
+    check_collision()
+    w.after(1000//FPS, update)
+
 def check_collision():
     if player.intersects(enemy):
         print('Танки столкнулись')
@@ -33,5 +40,7 @@ player = Tank(canvas = canv, x = 100, y = 50, ammo = 100)
 enemy = Tank(canvas = canv, x = 300, y = 300, ammo = 100)
 
 w.bind('<KeyPress>', key_press)
+
+update()
 
 w.mainloop()
--- 3/tank.py
+++ 3/tank.py
@@ -1,79 +1,129 @@
+from tkinter import *
 from hitbox import Hitbox
 class Tank:
 
-    count=0 # счетчик танков
-    SIZE = 100 # константа
+    __count=0 # счетчик танков
 
-    def __init__(self, canvas, x, y, model = 'T-14 Армата', ammo = 100, speed = 10):
-        Tank.count += 1
+    def __init__(self, canvas, x, y, model = 'T-14 Армата', ammo = 100, speed = 1,
+                 file_up = '../img/tankT34_up.png',
+                 file_down = '../img/tankT34_down.png',
+                 file_left = '../img/tankT34_left.png',
+                 file_right = '../img/tankT34_right.png'):
 
-        self.__hitbox = Hitbox(x, y, Tank.SIZE, Tank.SIZE)
+        self.__skin_up = PhotoImage(file = file_up)
+        self.__skin_down = PhotoImage(file = file_down)
+        self.__skin_left = PhotoImage(file = file_left)
+        self.__skin_right = PhotoImage(file = file_right)
 
-        self.canvas = canvas
-        self.model = model # модель
-        self.hp = 100 # здоровье
-        self.xp = 0 # опыт
-        self.ammo = ammo # боеприпасы
-        self.fuel = 100
-        self.speed = speed
-        self.x = x
-        self.y = y
+        Tank.__count += 1
 
-        if self.x < 0:
-            self.x = 0
-        if self.y < 0:
-            self.y = 0
+        self.__hitbox = Hitbox(x, y, self.get_sise(), self.get_sise())
 
-        self.create()
+        self.__canvas = canvas
+        self.__model = model # модель
+        self.__hp = 100 # здоровье
+        self.__xp = 0 # опыт
+        self.__ammo = ammo # боеприпасы
+        self.__fuel = 1000
+        self.__speed = speed
+        self.__x = x
+        self.__y = y
+
+        self.__vx = 0
+        self.__vy = 0
+
+        if self.__x < 0:
+            self.__x = 0
+        if self.__y < 0:
+            self.__y = 0
+
+        self.__create()
+        self.right()
 
     def fire(self):
-        if self.ammo > 0:
-            self.ammo -= 1
+        if self.__ammo > 0:
+            self.__ammo -= 1
             print('Стреляю')
 
-    def forward(self):
-        if self.fuel > 0:
-            self.y += self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
-            print(self)
+    def forward(self): # назад
+        if self.__fuel > 0:
+            self.__vx = 0
+            self.__vy = 1
+            self.__canvas.itemconfig(self.__id, image = self.__skin_down)
+            #self.__repaint()
 
-    def backvard(self):
-        if self.fuel > 0:
-            self.y -= self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
-            print(self)
+    def backvard(self): # вперёд
+        if self.__fuel > 0:
+            self.__vx = 0
+            self.__vy = -1
+            self.__canvas.itemconfig(self.__id, image = self.__skin_up)
+            #self.__repaint()
 
     def left(self):
-        if self.fuel > 0:
-            self.x -= self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
-            print(self)
+        if self.__fuel > 0:
+            self.__vx = -1
+            self.__vy = 0
+            self.__canvas.itemconfig(self.__id, image = self.__skin_left)
+            self.__repaint()
 
     def right(self):
-        if self.fuel > 0:
-            self.x += self.speed
+        if self.__fuel > 0:
+            self.__vx = 1
+            self.__vy = 0
+            self.__canvas.itemconfig(self.__id, image = self.__skin_right)
+            self.__repaint()
+
+    def update(self):
+        if self.__fuel > self.__speed:
+            self.__x += self.__vx * self.__speed
+            self.__y += self.__vy * self.__speed
+            self.__fuel -= self.__speed
             self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
-            print(self)
+            self.__repaint()
 
-    def create(self):
-        self.id = self.canvas.create_rectangle(self.x, self.y, self.x + Tank.SIZE, self.y + Tank.SIZE, fill = 'red')
+    def __create(self):
+        self.__id = self.__canvas.create_image(self.__x, self.__y, image = self.__skin_up, anchor = 'nw')
 
-    def repaint(self):
-        self.canvas.moveto(self.id, x = self.x, y = self.y)
+    def __repaint(self):
+        self.__canvas.moveto(self.__id, x = self.__x, y = self.__y)
 
     def __update_hitbox(self): # метод движения хитбокса
-        self.__hitbox.moveto(self.x, self.y)
+        self.__hitbox.moveto(self.__x, self.__y)
 
     def intersects(self, other_tank):
         return self.__hitbox.intersects(other_tank.__hitbox)
 
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def get_ammo(self):
+        return self.__ammo
+
+    def get_model(self):
+        return self.__model
+
+    def get_hp(self):
+        return self.__hp
+
+    def get_xp(self):
+        return self.__xp
+
+    def get_fuel(self):
+        return self.__fuel
+
+    def get_speed(self):
+        return self.__speed
+
+    @staticmethod
+    def get_quantity():
+        return Tank.__count
+
+    # @staticmethod
+    def get_sise(self):
+        return self.__skin_up.width()
+
     def __str__(self):
-        return (f'Модель: {self.model}, Здоровье: {self.hp}, Опыт: {self.xp}, Боеприпасы: {self.ammo}, Координаты: ({self.x}, {self.y})')
+        return (f'Модель: {self.__model}, Здоровье: {self.__hp}, Опыт: {self.__xp}, Боеприпасы: {self.__ammo}, Координаты: ({self.__x}, {self.__y})')
